0bf05941d368b9d2e64474233297e13d8c0e0c17
maxthestranger <thestrangermax@gmail.com>
2024-03-12 14:20:47 +0300

    initial setup

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/ussd', (req, res) => {
+    // Read the variables sent via POST from our API
+    const {
+        sessionId,
+        serviceCode,
+        phoneNumber,
+        text,
+    } = req.body;
+
+    let response = '';
+
+    if (text == '') {
+        // This is the first request. Note how we start the response with CON
+        response = `CON What would you like to check
+        1. List all routes
+        2. Search a route
+        3. Exit`;
+    } else if ( text == '1') {
+        // Get the list of all routes from the db
+        response = ``;
+    } else if ( text == '2') {
+        // Get the route by name/route number
+        response = `END Your phone number is ${phoneNumber}`;
+    } else if ( text == '1*1') {
+        // This is a second level response where the user selected 1 in the first instance
+        const accountNumber = 'ACC100101';
+        // This is a terminal request. Note how we start the response with END
+        response = `END Your account number is ${accountNumber}`;
+    }
+
+    // Send the response back to the API
+    res.set('Content-Type: text/plain');
+    res.send(response);
+});
