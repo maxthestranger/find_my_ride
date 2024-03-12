91018e95bae0235fa1b6c977bd2550ce8abf8c80
maxthestranger <thestrangermax@gmail.com>
2024-03-12 17:12:28 +0300

    add new

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Database connected'))
     .catch(err => console.error('Database connection error:', err));
